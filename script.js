84965cbd5c477761d59f5ac5a5ee9ce58ddb7f50
Gabriel Yara Fracaro <gabrielyarafracaro@icloud.com>
2025-08-10 23:31:56 -0300

    script.js

--- script.js
+++ script.js
@@ -70,9 +70,10 @@ var number = document.getElementsByClassName("number");
 for(var i =0;i<number.length;i++){
 	number[i].addEventListener('click',function(){
 		var output=reverseNumberFormat(getOutput());
-		if(output!=NaN){ //if output is a number
+		if(output!=NaN){ 
 			output=output+this.id;
 			printOutput(output);
 		}
 	});
+
 }
